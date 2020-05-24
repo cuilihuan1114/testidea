aec97381e146cee7cca599269e5cd410ab4bf8a3
cuilihuan <173@qq.com>
2020-05-24 09:56:05 +0800

    the thrid commitgit add .!

---
+++ src/main/java/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("test2");
+        System.out.println("aaaaa");
+        System.out.println("cccc");
+    }
+}
